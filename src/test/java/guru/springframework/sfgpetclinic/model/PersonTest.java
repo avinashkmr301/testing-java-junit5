ad0a72acb0653c1fe29f78cc427838342ea6a1e7
akum1176 <avinashkmr301@gmail.com>
2025-04-27 17:02:11 +0530

    grouped-assertions

---
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -0,0 +1,29 @@
+package guru.springframework.sfgpetclinic.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+
+    @Test
+    void groupedAssertions(){
+        //given
+        Person person = new Person(121L,"John","Sinha");
+        //then
+        assertAll("Test props set",
+                ()-> assertEquals("John", person.getFirstName()),
+                ()-> assertEquals("Sinha", person.getLastName()));
+    }
+
+    @Test
+    void groupedAssertionMsgs(){
+        //given
+        Person person = new Person(121L,"John","Sinha");
+        //then
+        assertAll("Test props set",
+                ()-> assertEquals("John", person.getFirstName(),"First name not matching"),
+                ()-> assertEquals("Sinha", person.getLastName(),"Last name not matching"));
+    }
+
+}
